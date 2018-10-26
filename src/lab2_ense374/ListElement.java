18bb4deb8018d5fb24fae6011ab717d28f8c3efb
lawal28a <44188104+lawal28a@users.noreply.github.com>
2018-10-26 13:01:11 -0600

    Used example given in assignment

--- src/lab2_ense374/ListElement.java
+++ src/lab2_ense374/ListElement.java
@@ -9,12 +9,20 @@ package lab2_ense374;
  */
 public class ListElement {
 
+
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private ListElement next ; 
+	private ListElement previous ; 
+	private int data ;
 
+	public ListElement () 
+	{ 
+		this . data = 0; 
+		this . next = null ; 
+		this . previous = null ; 
 	}
+
 
 }
