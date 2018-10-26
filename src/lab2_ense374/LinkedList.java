3e4cfc58a3d945b745849cffa00ad369aec50e40
lawal28a <44188104+lawal28a@users.noreply.github.com>
2018-10-26 12:49:25 -0600

    First commit of classes

---
+++ src/lab2_ense374/LinkedList.java
@@ -0,0 +1,10 @@
+package lab2_ense374;
+
+public class LinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//fhhhffy
+	}
+
+}
---
+++ src/lab2_ense374/ListElement.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package lab2_ense374;
+
+/**
+ * @author lawal28a
+ *
+ */
+public class ListElement {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
