950b9b19a08ae02b17c0b43a3d06eada0c87b813
lawal28a <44188104+lawal28a@users.noreply.github.com>
2018-10-27 17:39:13 -0600

    removed some files and added to the LinkedList class

--- src/lab2_ense374/ListElement.java
+++ src/lab2_ense374/ListElement.java
@@ -8,47 +8,25 @@ package lab2_ense374;
  *
  */
 public class ListElement {
-
-
-	/**
-	 * @param args
-	 */
+	
 	private ListElement next ; 
 	private ListElement previous ; 
 	private int data ;
 
 	public ListElement () 
 	{ 
-		this . data = 0; 
-		this . next = null ; 
-		this . previous = null ; 
+		this.data = 0; 
+		this.next = null ; 
+		this.previous = null ; 
 	}
 
-	/** @param data 
-	* an integer to be stored in the node 
-	* @return 
-	* * None
-	**/ 
 	public void setData ( int data) { 
-		this . data = data ; 
+		this.data = data ; 
 	} 
+	
 	public int getData () { 
-		return this . data ; 
+		return this.data ; 
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ListElement le = new ListElement(); 
-		//create an object of the class, and access the members
-		le.setData(5);
-	}
 	
-	public void addElement(ListElement le) {
-		 if (this.next == null)
-	        {
-	            this.next = le;
-	        }
-	}
-	
-
 }
